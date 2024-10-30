5384cfc79057ea5930cdf205862800f30be7c06a
Arthur Cristo <arthur.cristo2804@gmail.com>
2024-10-30 19:00:21 -0300

    refactor: Consolidate Dashboard layout into a single component and enhance balance display

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,10 +1,24 @@
-import { cookies } from 'next/headers'
 import { createClient } from '@/lib/supabase/server'
 import { redirect } from 'next/navigation'
-import DashboardClient from '@/components/Dashboard'
 import Header from '@/components/Header'
+import { HandCoins, Wallet, MessageCircleMore, PiggyBank, ShoppingBag } from "lucide-react"
+import DashboardNewsWindow from '@/components/DashboardNewsWindow'
+import DashboardOption from '@/components/DashboardOption'
+import { Button } from '@/components/ui/button'
 
 export default async function DashboardPage() {
+
+  const USDollar = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+  })
+
+  const options = [
+    { label: 'Transactions', link: '/', Icon: HandCoins },
+    { label: 'Stocks', link: '/', Icon: Wallet },
+    { label: 'Store', link: '/', Icon: ShoppingBag },
+    { label: 'Community', link: '/', Icon: MessageCircleMore },
+  ]
 
   const supabase = createClient()
 
@@ -18,10 +32,35 @@ export default async function DashboardPage() {
     .eq('id', user.id)
     .single();
 
+  const { balance } = profile
+
   return (
     <div className='bg-dark_gray-gradient text-white text-center min-h-screen'>
       <Header />
-      <DashboardClient profile={profile} />
+      <div className="md:flex md:justify-between md:items-center md:mt-10">
+        <div className="md:w-2/3 md:flex md:flex-col md:items-center">
+          <div className="md:bg-register-card-gradient md:p-6 md:w-[400px] md:mb-6 md:rounded-md flex flex-col gap-4 justify-center p-8 py-12">
+            <h2 className="text-2xl text-gray-200">Your Balance</h2>
+            <h1 className="font-bold text-4xl">
+              <span className="text-green-500">A$ </span>
+              {USDollar.format(balance).slice(1)}
+            </h1>
+            <h3 className="text-sm text-gray-300 underline cursor-pointer">View Bank Statement</h3>
+          </div>
+          <div className="flex justify-center ">
+            <Button className="flex my-8 py-6 mx-8 md:w-[400px] w-full">
+              <PiggyBank className="h-10 w-10 mr-2" />
+              My Savings
+            </Button>
+          </div>
+          <div className="md:bg-transparent px-3 flex gap-6 items-center my-8 py-5 bg-dark-gray md:w-fit w-screen overflow-x-auto">
+            {options.map((option, index) => (
+              <DashboardOption key={index} {...option} />
+            ))}
+          </div>
+        </div>
+        <DashboardNewsWindow />
+      </div>
     </div>
   )
 }
