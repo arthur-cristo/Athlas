d88d4f03250464dee61deacbd5773611645c84a0
Arthur Cristo <arthur.cristo2804@gmail.com>
2024-11-12 18:52:31 -0300

    fix: Ensure balance fetch only occurs for authenticated users and conditionally render link to bank statement

--- components/BalanceDisplay.tsx
+++ components/BalanceDisplay.tsx
@@ -5,18 +5,20 @@ import { dollarFormat } from '@/lib/utils'
 import { useEffect, useState } from 'react'
 import Link from 'next/link'
 import { useUser } from '@/app/UserContext'
+import { usePathname } from 'next/navigation'
 
 const BalanceDisplay = () => {
 
     const [balance, setBalance] = useState(0);
     const user = useUser();
     const supabase = createClient();
+    const pathName = usePathname();
 
     useEffect(() => {
-        if (!user) return;
 
         const fetchBalance = async () => {
             try {
+                if (!user) return;
                 const { data, error } = await supabase
                     .from('profiles')
                     .select('balance')
@@ -35,7 +37,7 @@ const BalanceDisplay = () => {
         const intervalId = setInterval(fetchBalance, 10000);
 
         return () => clearInterval(intervalId);
-    }, []);
+    }, [user]);
 
     return (
         <div className="bg-balance_display-gradient md:bg-transparent md:p-8 md:w-[400px] md:mb-6 md:rounded-md flex flex-col gap-4 justify-center p-8 py-16">
@@ -44,9 +46,11 @@ const BalanceDisplay = () => {
                 <span className="text-green-500">$</span>
                 {dollarFormat.format(balance).slice(1)}
             </h1>
-            <Link href='/transactions'>
-                <h3 className="text-sm text-gray-300 underline cursor-pointer">View Bank Statement</h3>
-            </Link>
+            {pathName !== '/transactions' && (
+                <Link href='/transactions'>
+                    <h3 className="text-sm text-gray-300 underline cursor-pointer">View Bank Statement</h3>
+                </Link>
+            )}
 
         </div>
     )
