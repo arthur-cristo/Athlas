ca2ef243a983ea11a8c814656efbd3e55731a0ac
Arthur Cristo <arthur.cristo2804@gmail.com>
2024-11-18 17:59:19 -0300

    fix: fix a bug where it wasnt possible to search the user by his random key, that happened because of the order of the input and input type

--- app/api/users/search/route.ts
+++ app/api/users/search/route.ts
@@ -9,9 +9,8 @@ export async function GET(request: NextRequest) {
     const id = searchParams.get('id');
     const phone_number = '+' + searchParams.get('phone_number')?.slice(1);
     const random_key = searchParams.get('random_key');
-    const input = email || id || phone_number || random_key;
-    const inputType = email ? 'email' : id ? 'id' : phone_number ? 'phone_number' : 'random_key';
-
+    const input = email || id || random_key || phone_number;
+    const inputType = email ? 'email' : id ? 'id' : random_key ? 'random_key' : 'phone_number';
     if (!input) return NextResponse.json({ error: "No search input provided" }, { status: 400 });
     const { data, error } = await supabase.from('profiles').select('*, profile_follows!profile_follows_following_fkey(follower)').eq(inputType, input).single();
     if (!data || error) return NextResponse.json({ error: "The user wasn't found" }, { status: 404 });
