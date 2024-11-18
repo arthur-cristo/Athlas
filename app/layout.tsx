ab99823e8e8c4cb4e1f72c0c5ba41b8a5d703d9d
Arthur Cristo <arthur.cristo2804@gmail.com>
2024-11-18 16:23:05 -0300

    feat: add toast notification system with Toaster component

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import localFont from "next/font/local";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 import { UserProvider } from "./UserContext";
+import { Toaster } from "@/components/ui/toaster";
 
 export const metadata: Metadata = {
   title: "Athlas",
@@ -20,6 +21,7 @@ export default function RootLayout({
         <ThemeProvider>
           <UserProvider>
             {children}
+            <Toaster />
           </UserProvider>
         </ThemeProvider>
       </body>
