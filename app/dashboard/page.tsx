47831254e820d08c532fc8be52697d1518e16059
Arthur Cristo <arthur.cristo2804@gmail.com>
2024-12-26 11:21:57 -0300

    style: improve dashboard style

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -10,23 +10,20 @@ import BalanceDisplay from '@/components/BalanceDisplay'
 export default async function DashboardPage() {
 
   return (
-    <div className='text-center min-h-screen'>
-      
-      <main className="flex flex-col justify-center items-center md:pt-0 pt-32">
-        <div className="mt-4 flex flex-col items-center justify-center">
-          <BalanceDisplay />
-          <Button className="flex my-4 mx-8 py-6 mt-0 md:w-[400px] px-20 text-xl">
-            <PiggyBank className="h-[32px] w-[32px] mr-2" />
-            Meu Porquinho
-          </Button>
-          <div className="md:w-[400px] md:flex-wrap md:overflow-x-auto overflow-x-scroll justify-start px-3 flex gap-6 items-center md:my-0 my-8 py-5 bg-muted dark:bg-muted/50 dark:md:bg-transparent md:bg-background w-screen md:justify-center text-foreground">
-            {dashboardOptions.map((option, index) => (
-              <DashboardOption key={index} {...option} />
-            ))}
-          </div>
+    <main className="flex flex-col justify-center items-center md:pt-0 pt-32 mt-8 text-center overflow-hidden">
+      <div className="mt-4 flex flex-col items-center justify-center">
+        <BalanceDisplay />
+        <Button className="flex my-4 mx-8 py-6 mt-0 md:w-[400px] px-20 text-xl">
+          <PiggyBank className="h-[32px] w-[32px] mr-2" />
+          Meu Porquinho
+        </Button>
+        <div className="ml-4 md:ml-0 md:w-[400px] md:flex-wrap md:overflow-x-auto overflow-x-scroll justify-start px-3 flex gap-6 items-center md:my-0 my-8 py-5 bg-muted dark:bg-muted/50 dark:md:bg-transparent md:bg-background w-screen md:justify-center text-foreground">
+          {dashboardOptions.map((option, index) => (
+            <DashboardOption key={index} {...option} />
+          ))}
         </div>
-        <DashboardNewsWindow />
-      </main>
-    </div>
+      </div>
+      <DashboardNewsWindow />
+    </main>
   )
 }
--- lib/constants.ts
+++ lib/constants.ts
@@ -1,10 +1,7 @@
-import { GraduationCap, HandCoins, KeyRound, MessageCircleMore, PiggyBank, ShoppingBag, Wallet } from "lucide-react";
+import { GraduationCap, HandCoins, KeyRound, MessageCircleMore } from "lucide-react";
 
 export const dashboardOptions = [
     { label: 'Transações', link: '/transactions', Icon: HandCoins },
-    /* { label: 'Stocks', link: '/stocks/', Icon: Wallet, disabled: true },
-    { label: 'Store', link: '/store/', Icon: ShoppingBag, disabled: true }, */
-    { label: 'Porquinho', link: '/piggy-bank/', Icon: PiggyBank, disabled: true },
     { label: 'Aprender', link: '/learn/', Icon: GraduationCap },
     { label: 'Comunidade', link: '/community/', Icon: MessageCircleMore },
 ]
